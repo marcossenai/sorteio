55f19d0d5755cce413ebb1fb4756d4b4179aea3c
marcossenai <133662787+marcossenai@users.noreply.github.com>
2023-08-07 13:46:16 -0300

    Initial commit

---
+++ aula.js
@@ -0,0 +1,18 @@
+//  Math.random gera um número aleatótio
+console.log(Math.random());
+
+// Gerando um número aleatório entre 0 e 20
+console.log(Math.random() * 20);
+console.log(Math.round(Math.random()*5));
+
+// Math.round arredonda um número para o inteiro mais próximo
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+// Math.ceil arredonda para cima
+console.log(Math.ceil(3.1));
+console.log(Math.ceil(3.9));
+
+// Math.floor arredondando para baixo
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9));
